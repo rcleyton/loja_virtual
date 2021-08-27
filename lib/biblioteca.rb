ef052ab3081d01e7be610d26593210af647f2375
Cleyton Silva <silva.rcleyton@gmail.com>
2021-08-27 10:42:06 -0300

    Add require files

--- lib/biblioteca.rb
+++ lib/biblioteca.rb
@@ -1,11 +1,22 @@
 # coding: utf-8
 
 class Biblioteca
+  attr_reader :livros
+
   def initialize
-    @livros = []
+    @livros = {}
   end
 
   def adiciona(livro)
-    @livros << livro
+    @livros[livro.categoria] ||= []
+    @livros[livro.categoria] << livro
+  end
+
+  def livros
+    @livros.values.flatten
+  end
+
+  def livros_por_categoria(categoria)
+    @livros[categoria]
   end
 end
--- lib/livro.rb
+++ lib/livro.rb
@@ -2,16 +2,18 @@
 
 class Livro
   attr_accessor :preco
+  attr_reader :categoria
   
-  def initialize(autor, isbn = '1', numero_de_paginas, preco)
+  def initialize(autor, isbn = '1', numero_de_paginas, preco, categoria)
     @autor = autor
     @isbn = isbn
     @numero_de_paginas = numero_de_paginas
     @preco = preco
+    @categoria
   end
 
   def to_s
-    "Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}"
+    "Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}, Categoria: #{@categoria}"
   end
 
 end
